dc45f04851d0e60cc46154f3d593ead77f143c5c
VinayMavi <vinaymavi@gmail.com>
2017-07-22 21:53:58 +0530

    PROGRESS UPDATED.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+console.log(ReactDOMServer.renderToStaticMarkup(<App/>));
 registerServiceWorker();
